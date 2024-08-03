9c2ef9b7ae7c69010e331c5f9273abe508785cc1
Suraj Meena <surajmeena505@gmail.com>
2024-08-03 13:42:28 +0530

    Refactor TaskComponent and related files

--- src/app/task/task.component.spec.ts
+++ src/app/task/task.component.spec.ts
@@ -1,18 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ViewTaskComponent } from './task.component';
+import { TaskComponent } from './task.component';
 
 describe('ViewTaskComponent', () => {
-  let component: ViewTaskComponent;
-  let fixture: ComponentFixture<ViewTaskComponent>;
+  let component: TaskComponent;
+  let fixture: ComponentFixture<TaskComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ViewTaskComponent]
-    })
-    .compileComponents();
+      imports: [TaskComponent],
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(ViewTaskComponent);
+    fixture = TestBed.createComponent(TaskComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
